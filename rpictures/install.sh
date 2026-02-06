4544c803cfdb681379bdce1d84b89635d5e4bd81
QuiquempoisPaul <92971845+Loghin01@users.noreply.github.com>
2026-02-06 11:58:48 +0100

    fix: install.sh rpicture (#89)
    * Update apps.json and ports.json
    
    * Include LDAP sync in install script
    
    Add LDAP sync call after server startup check.
    
    * chore: auto-increment app versions [skip ci]
    
    * Update apps.json and ports.json
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github-actions>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- rpictures/install.sh
+++ rpictures/install.sh
@@ -72,6 +72,9 @@ until curl -s http://localhost:3013 > /dev/null; do
     sleep 2
     echo -n "."
 done
+
+curl http://localhost:3013/api/ldap/sync
+
 echo ""
 echo "✅ rPictures est lancé."
 echo "ℹ️ Note: La synchronisation LDAP se fera après la création du premier utilisateur."
